430013b4342fd43625968acf7bff9c88bf0d3f9b
Ramya <ramya.fsd@gmail.com>
2023-11-03 11:34:32 +0530

    Episode02 chnages

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from "react";
+import reactDom from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h1", {}, "I am Ramya katta"),
     React.createElement("h2", {}, "I am an h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
@@ -8,5 +11,5 @@ const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("h2", {}, "I am an h2 tag"),
   ]),
 ]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = reactDom.createRoot(document.getElementById("root"));
 root.render(parent);
---
+++ README.md
@@ -0,0 +1,17 @@
+# parcel
+Dev build
+Local server
+HMR - Hot Module Replacement
+File watching algorithm - written in c++
+Faster build because of caching
+Image optimization
+Minification
+compressing
+Bundling
+Consistent Hashing
+Differential bundling - to support older browsers
+Error handling
+Tree shaking algorithm - Rmoves unused code
+Different dev and prod builds
+
+ 
